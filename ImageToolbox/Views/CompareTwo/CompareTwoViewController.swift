e81cd81d81a1149db8baf5af26fa56713a48b01f
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-01-19 14:58:47 +0100

    Updated the UI of "CompareTwoViewController". Added two UIImageViews to manage the action of selecting and displaying a picture. Updated the logic of "CompareTwoViewController". Now the user can select a couple of photos from the Photos Library to compare them.

--- ImageToolbox/Views/CompareTwo/CompareTwoViewController.swift
+++ ImageToolbox/Views/CompareTwo/CompareTwoViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PhotosUI // To display and select pictures from  Photos Library.
 
 class CompareTwoViewController: UIViewController {
     
@@ -18,7 +19,28 @@ class CompareTwoViewController: UIViewController {
         changeTopImageViewOpacity(slider: sender as! UISlider)
     }
     
+    @IBOutlet weak var bottomSelectedPhotoImageView: UIImageView!
+    @IBOutlet weak var topSelectedPhotoImageView: UIImageView!
+    
     // MARK: - Properties
+    
+    enum PicturePosition {
+        case bottom
+        case top
+    }
+    
+    var photoPickerConf: PHPickerConfiguration {
+        var phPickerConf = PHPickerConfiguration()
+        phPickerConf.selectionLimit = 1
+        phPickerConf.filter = PHPickerFilter.any(of: [.images, .screenshots])
+        return phPickerConf
+    }
+    var photoPickerVC: PHPickerViewController {
+        let phPickerVC = PHPickerViewController(configuration: photoPickerConf)
+        phPickerVC.delegate = self
+        return phPickerVC
+    }
+    var selectedImageViewForPictureDisplay: PicturePosition = .bottom
     
     // MARK: - Methods
     
@@ -31,15 +53,84 @@ class CompareTwoViewController: UIViewController {
     }
     
     private func setupView() {
+        // UIImageViews
         bottomImageView.backgroundColor = UIColor.red
         topImageView.backgroundColor = UIColor.blue
         
+        setupPictureSelectionImageViews()
+        
+        // UISlider
         opacitySlider.value = 0
         topImageView.layer.opacity = opacitySlider.value
     }
     
+    private func setupPictureSelectionImageViews() {
+        // Corner Radius.
+        bottomSelectedPhotoImageView.layer.cornerRadius = 4
+        topSelectedPhotoImageView.layer.cornerRadius = 4
+        
+        // Border.
+        bottomSelectedPhotoImageView.layer.borderWidth = 1
+        bottomSelectedPhotoImageView.layer.borderColor = UIColor.black.cgColor
+        
+        topSelectedPhotoImageView.layer.borderWidth = 1
+        topSelectedPhotoImageView.layer.borderColor = UIColor.black.cgColor
+        
+        // Content mode.
+        bottomSelectedPhotoImageView.contentMode = .scaleAspectFill
+        topSelectedPhotoImageView.contentMode = .scaleAspectFill
+        
+        // Gesture Recognizers.
+        let selectBottomPhotoGR = UITapGestureRecognizer(target: self, action: #selector(selectBottomPicture))
+        let selectTopPhotoGR = UITapGestureRecognizer(target: self, action: #selector(selectTopPicture))
+        
+        bottomSelectedPhotoImageView.addGestureRecognizer(selectBottomPhotoGR)
+        bottomSelectedPhotoImageView.isUserInteractionEnabled = true
+        
+        topSelectedPhotoImageView.addGestureRecognizer(selectTopPhotoGR)
+        topSelectedPhotoImageView.isUserInteractionEnabled = true
+    }
+    
     private func changeTopImageViewOpacity(slider: UISlider) {
         topImageView.layer.opacity = slider.value
+    }
+    
+    @objc private func selectBottomPicture() {
+        selectedImageViewForPictureDisplay = .bottom
+        present(photoPickerVC, animated: true)
+    }
+    
+    @objc private func selectTopPicture() {
+        selectedImageViewForPictureDisplay = .top
+        present(photoPickerVC, animated: true)
+    }
+    
+}
+
+// PhotosUI, PHPickerViewControllerDelegate
+
+extension CompareTwoViewController: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        results.forEach { result in
+            result.itemProvider.loadObject(ofClass: UIImage.self) { reading, error in
+                guard let image = reading as? UIImage,
+                      error == nil else { return }
+                
+                DispatchQueue.main.async {
+                    switch self.selectedImageViewForPictureDisplay {
+                    case .bottom:
+                        self.bottomImageView.image = image
+                        self.bottomSelectedPhotoImageView.image = image
+                    case .top:
+                        self.topImageView.image = image
+                        self.topSelectedPhotoImageView.image = image
+                    }
+                }
+            }
+        }
     }
     
 }
