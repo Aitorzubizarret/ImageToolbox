a6c7ae4cd6716d12810e2c525b7a3ed24be1d4b0
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-01-25 15:27:46 +0100

    Updated "CompareTwoViewController" to add the logic to zoom in and out (using pinch gesture) both picture at the same time.

--- ImageToolbox/Views/CompareTwo/CompareTwoViewController.swift
+++ ImageToolbox/Views/CompareTwo/CompareTwoViewController.swift
@@ -42,10 +42,30 @@ class CompareTwoViewController: UIViewController {
         phPickerVC.delegate = self
         return phPickerVC
     }
+    
     var selectedImageViewForPictureDisplay: PicturePosition = .bottom
     
     var bottomPictureLayer = CALayer()
     var topPictureLayer = CALayer()
+    
+    var bottomPicture: UIImage?
+    var topPicture: UIImage?
+    
+    var bottomPictureOriginalScale: CGFloat = 1
+    var bottomPictureCurrentScale: CGFloat = 1 {
+        didSet {
+            resizeDisplayedPictures(picturePosition: .bottom)
+        }
+    }
+    var topPictureOriginalScale: CGFloat = 1
+    var topPictureCurrentScale: CGFloat = 1 {
+        didSet {
+            resizeDisplayedPictures(picturePosition: .top)
+        }
+    }
+    
+    var frameWidth: CGFloat = 0
+    var frameHeight: CGFloat = 0
     
     // MARK: - Methods
     
@@ -59,6 +79,9 @@ class CompareTwoViewController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
+        frameWidth = imageView.frame.width
+        frameHeight = imageView.frame.height
+        
         setupControlsView()
     }
     
@@ -66,6 +89,11 @@ class CompareTwoViewController: UIViewController {
         // UISlider
         opacitySlider.value = 0
         topPictureLayer.opacity = opacitySlider.value
+        
+        // Pinch Gesture Recognizer.
+        imageView.isUserInteractionEnabled = true
+        let pinchGR = UIPinchGestureRecognizer(target: self, action: #selector(pinchImage(sender: )))
+        imageView.addGestureRecognizer(pinchGR)
     }
     
     private func setupControlsView() {
@@ -128,56 +156,81 @@ class CompareTwoViewController: UIViewController {
             print("")
         }
         
+        switch self.selectedImageViewForPictureDisplay {
+        case .bottom:
+            bottomPicture = currentPicture
+        case .top:
+            topPicture = currentPicture
+        }
+        
         // Sizes.
         let pictureWidth: CGFloat = currentPicture.size.width
         let pictureHeight: CGFloat = currentPicture.size.height
-        let frameWidth: CGFloat = imageView.frame.width
-        let frameHeight: CGFloat = imageView.frame.height
         
         let widthScale: CGFloat = pictureWidth / frameWidth
         let heightScale: CGFloat = pictureHeight / frameHeight
-        var scale: CGFloat = 0
         
         // Checks which scale value is better for the picture, taking into account the frame size.
         if (pictureWidth / widthScale <= frameWidth) && (pictureHeight / widthScale <= frameHeight) {
-            scale = widthScale
+            switch selectedImageViewForPictureDisplay {
+            case .bottom:
+                bottomPictureOriginalScale = widthScale
+                bottomPictureCurrentScale = widthScale
+            case .top:
+                topPictureOriginalScale = widthScale
+                topPictureCurrentScale = widthScale
+            }
         } else {
-            scale = heightScale
+            switch selectedImageViewForPictureDisplay {
+            case .bottom:
+                bottomPictureOriginalScale = heightScale
+                bottomPictureCurrentScale = heightScale
+            case .top:
+                topPictureOriginalScale = heightScale
+                topPictureCurrentScale = heightScale
+            }
         }
         
-        switch self.selectedImageViewForPictureDisplay {
+    }
+    
+    private func resizeDisplayedPictures(picturePosition: PicturePosition) {
+        switch picturePosition {
         case .bottom:
-            // Bottom Picture Layer.
-            bottomPictureLayer.frame = CGRect(x: 0,
-                                              y: 0,
-                                              width: frameWidth,
-                                              height: frameHeight)
-            bottomPictureLayer.contents = currentPicture.cgImage
-            bottomPictureLayer.contentsGravity = .center
-            bottomPictureLayer.contentsScale = scale
-            
-            imageView.layer.addSublayer(bottomPictureLayer)
-            
-            // Top Picture Layer.
-            imageView.layer.addSublayer(topPictureLayer)
-            
-            self.bottomSelectedPhotoImageView.image = currentPicture
+            if let bottomPicture = bottomPicture {
+                // Bottom Picture Layer.
+                bottomPictureLayer.frame = CGRect(x: 0,
+                                                  y: 0,
+                                                  width: frameWidth,
+                                                  height: frameHeight)
+                bottomPictureLayer.contents = bottomPicture.cgImage
+                bottomPictureLayer.contentsGravity = .center
+                bottomPictureLayer.contentsScale = bottomPictureCurrentScale
+                
+                imageView.layer.addSublayer(bottomPictureLayer)
+                
+                // Top Picture Layer.
+                imageView.layer.addSublayer(topPictureLayer)
+                
+                self.bottomSelectedPhotoImageView.image = bottomPicture
+            }
         case .top:
-            // Bottom Picture Layer.
-            imageView.layer.addSublayer(bottomPictureLayer)
-            
-            // Top Picture Layer.
-            topPictureLayer.frame = CGRect(x: 0,
-                                           y: 0,
-                                           width: frameWidth,
-                                           height: frameHeight)
-            topPictureLayer.contents = currentPicture.cgImage
-            topPictureLayer.contentsGravity = .center
-            topPictureLayer.contentsScale = scale
-            
-            imageView.layer.addSublayer(topPictureLayer)
-            
-            self.topSelectedPhotoImageView.image = currentPicture
+            if let topPicture = topPicture {
+                // Bottom Picture Layer.
+                imageView.layer.addSublayer(bottomPictureLayer)
+                
+                // Top Picture Layer.
+                topPictureLayer.frame = CGRect(x: 0,
+                                               y: 0,
+                                               width: frameWidth,
+                                               height: frameHeight)
+                topPictureLayer.contents = topPicture.cgImage
+                topPictureLayer.contentsGravity = .center
+                topPictureLayer.contentsScale = topPictureCurrentScale
+                
+                imageView.layer.addSublayer(topPictureLayer)
+                
+                self.topSelectedPhotoImageView.image = topPicture
+            }
         }
     }
     
@@ -189,6 +242,35 @@ class CompareTwoViewController: UIViewController {
     @objc private func selectTopPicture() {
         selectedImageViewForPictureDisplay = .top
         present(photoPickerVC, animated: true)
+    }
+    
+    @objc private func pinchImage(sender: UIPinchGestureRecognizer) {
+        if sender.state == .began || sender.state == .changed {
+            /// The value received by the PinchGR, makes the image smaller when zoom-in, and bigger when zoom-out.
+            /// To "fix" this :
+            /// Zoom-in   => Received value 1,014 => Correct value => 0,986 (Smaller than 1) => 2 - 1,014
+            /// Zoom-out => Received value 0,98   => Correct value => 1,02 (Bigger than 1) => 2 - 0,98
+            var newBottomPictureScale = bottomPictureCurrentScale * (2 - sender.scale)
+            var newTopPictureScale = topPictureCurrentScale * (2 - sender.scale)
+            
+            if newBottomPictureScale < 1 {
+                newBottomPictureScale = 1
+            }
+            if newTopPictureScale < 1 {
+                newTopPictureScale = 1
+            }
+            
+            if newBottomPictureScale > (bottomPictureOriginalScale + 2) {
+                newBottomPictureScale = bottomPictureOriginalScale + 2
+            }
+            if newTopPictureScale > (topPictureOriginalScale + 2) {
+                newTopPictureScale = topPictureOriginalScale + 2
+            }
+            
+            bottomPictureCurrentScale = newBottomPictureScale
+            topPictureCurrentScale = newTopPictureScale
+            sender.scale = 1
+        }
     }
     
 }
