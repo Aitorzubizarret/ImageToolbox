4e9d85279780ba906b82d75c719573d3a2606a47
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-01-26 12:40:28 +0100

    Updated "CompareTwoViewController". Deleted the "bottomPicture" and "topPicture" properties and added an array of Pictures. Changed the logic to display and scale the pictures with this new array.

--- ImageToolbox/Views/CompareTwoViewController/CompareTwoViewController.swift
+++ ImageToolbox/Views/CompareTwoViewController/CompareTwoViewController.swift
@@ -27,11 +27,6 @@ class CompareTwoViewController: UIViewController {
     
     // MARK: - Properties
     
-    enum PicturePosition {
-        case bottom
-        case top
-    }
-    
     var photoPickerConf: PHPickerConfiguration {
         var phPickerConf = PHPickerConfiguration()
         phPickerConf.selectionLimit = 1
@@ -44,10 +39,7 @@ class CompareTwoViewController: UIViewController {
         return phPickerVC
     }
     
-    var selectedImageViewForPictureDisplay: PicturePosition = .bottom
-    
-    var bottomPicture: Picture?
-    var topPicture: Picture?
+    var pictures: [Picture] = []
     
     var topLayerOpacity: Float = 0 {
         didSet {
@@ -123,101 +115,92 @@ class CompareTwoViewController: UIViewController {
     }
     
     private func changeTopImageViewOpacity() {
-        guard let topPicture = topPicture else { return }
-        
-        topPicture.layer.opacity = topLayerOpacity
+        if pictures.count == 2 {
+            let topPicture = pictures[1]
+            topPicture.layer.opacity = topLayerOpacity
+        }
     }
     
     private func displayPictureInImageView(picture: UIImage) {
         let currentPicture = Picture(image: picture, frameWidth: imageView.frame.width, frameHeight: imageView.frame.height)
         
+        pictures.append(currentPicture)
+        
         // Delete all previous layers.
         imageView.layer.sublayers?.removeAll()
         
-        switch self.selectedImageViewForPictureDisplay {
-        case .bottom:
-            bottomPicture = currentPicture
-        case .top:
-            topPicture = currentPicture
-        }
-        
-        resizeDisplayedPictures(picturePosition: .bottom)
-        resizeDisplayedPictures(picturePosition: .top)
+        resizeDisplayedPictures()
     }
     
-    private func resizeDisplayedPictures(picturePosition: PicturePosition) {
-        switch picturePosition {
-        case .bottom:
-            if let bottomPicture = bottomPicture {
-                bottomPicture.layer.contentsScale = bottomPicture.currentScale
-                imageView.layer.addSublayer(bottomPicture.layer)
-                
-                // Top Picture Layer.
-                if let topPicture = topPicture {
-                    imageView.layer.addSublayer(topPicture.layer)
-                }
-                
-                self.bottomSelectedPhotoImageView.image = bottomPicture.image
-            }
-        case .top:
-            if let topPicture = topPicture {
-                // Bottom Picture Layer.
-                if let bottomPicture = bottomPicture {
-                    imageView.layer.addSublayer(bottomPicture.layer)
-                }
-                
-                topPicture.layer.opacity = topLayerOpacity
-                topPicture.layer.contentsScale = topPicture.currentScale
-                imageView.layer.addSublayer(topPicture.layer)
-                
-                self.topSelectedPhotoImageView.image = topPicture.image
-            }
+    private func resizeDisplayedPictures() {
+        
+        switch pictures.count {
+        case 1:
+            let bottomPicture = pictures[0]
+            bottomPicture.layer.contentsScale = bottomPicture.currentScale
+            
+            imageView.layer.addSublayer(bottomPicture.layer)
+            bottomSelectedPhotoImageView.image = bottomPicture.image
+        case 2:
+            let bottomPicture = pictures[0]
+            bottomPicture.layer.contentsScale = bottomPicture.currentScale
+            
+            imageView.layer.addSublayer(bottomPicture.layer)
+            bottomSelectedPhotoImageView.image = bottomPicture.image
+            
+            let topPicture = pictures[1]
+            topPicture.layer.contentsScale = topPicture.currentScale
+            topPicture.layer.opacity = topLayerOpacity
+            
+            imageView.layer.addSublayer(topPicture.layer)
+            topSelectedPhotoImageView.image = topPicture.image
+        default:
+            print("")
         }
     }
     
     @objc private func selectBottomPicture() {
-        selectedImageViewForPictureDisplay = .bottom
         present(photoPickerVC, animated: true)
     }
     
     @objc private func selectTopPicture() {
-        selectedImageViewForPictureDisplay = .top
         present(photoPickerVC, animated: true)
     }
     
     @objc private func pinchImage(sender: UIPinchGestureRecognizer) {
         if sender.state == .began || sender.state == .changed {
-            guard let bottomPicture = bottomPicture,
-                  let topPicture = topPicture else { return }
-            
-            /// The value received by the PinchGR, makes the image smaller when zoom-in, and bigger when zoom-out.
-            /// To "fix" this :
-            /// Zoom-in   => Received value 1,014 => Correct value => 0,986 (Smaller than 1) => 2 - 1,014
-            /// Zoom-out => Received value 0,98   => Correct value => 1,02 (Bigger than 1) => 2 - 0,98
-            var newBottomPictureScale = bottomPicture.currentScale * (2 - sender.scale)
-            var newTopPictureScale = topPicture.currentScale * (2 - sender.scale)
-            
-            if newBottomPictureScale < 1 {
-                newBottomPictureScale = 1
+            if pictures.count == 2 {
+                let bottomPicture = pictures[0]
+                let topPicture = pictures[1]
+                
+                /// The value received by the PinchGR, makes the image smaller when zoom-in, and bigger when zoom-out.
+                /// To "fix" this :
+                /// Zoom-in   => Received value 1,014 => Correct value => 0,986 (Smaller than 1) => 2 - 1,014
+                /// Zoom-out => Received value 0,98   => Correct value => 1,02 (Bigger than 1) => 2 - 0,98
+                var newBottomPictureScale = bottomPicture.currentScale * (2 - sender.scale)
+                var newTopPictureScale = topPicture.currentScale * (2 - sender.scale)
+                
+                if newBottomPictureScale < 1 {
+                    newBottomPictureScale = 1
+                }
+                if newTopPictureScale < 1 {
+                    newTopPictureScale = 1
+                }
+                
+                if newBottomPictureScale > (bottomPicture.originalScale + 2) {
+                    newBottomPictureScale = bottomPicture.originalScale + 2
+                }
+                if newTopPictureScale > (topPicture.originalScale + 2) {
+                    newTopPictureScale = topPicture.originalScale + 2
+                }
+                
+                bottomPicture.currentScale = newBottomPictureScale
+                topPicture.currentScale = newBottomPictureScale
+                
+                sender.scale = 1
+                
+                resizeDisplayedPictures()
             }
-            if newTopPictureScale < 1 {
-                newTopPictureScale = 1
-            }
-            
-            if newBottomPictureScale > (bottomPicture.originalScale + 2) {
-                newBottomPictureScale = bottomPicture.originalScale + 2
-            }
-            if newTopPictureScale > (topPicture.originalScale + 2) {
-                newTopPictureScale = topPicture.originalScale + 2
-            }
-            
-            bottomPicture.currentScale = newBottomPictureScale
-            topPicture.currentScale = newBottomPictureScale
-            
-            sender.scale = 1
-            
-            resizeDisplayedPictures(picturePosition: .bottom)
-            resizeDisplayedPictures(picturePosition: .top)
         }
     }
     
